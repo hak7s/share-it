ec3d93ecf10bcb6aac3a13e5138dea8aa314eb2e
Hakim <hak7s@hotmail.fr>
2020-11-25 13:57:11 +0100

    telecharger un fichier

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,9 +10,14 @@ class HomeController extends AbstractController
 {
     public function homepage(ResponseInterface $response, Connection $connection, ServerRequestInterface $request)
     {
-        $database = $connection->getDatabase();
         $files = $request->getUploadedFiles();
-        var_dump($files);
+        if (!empty($files['fichier'])) {
+            $newfile = $files['fichier'];
+            if ($newfile->getError() === UPLOAD_ERR_OK) {
+                $uploadFileName = $newfile->getClientFilename();
+                $newfile->moveTo("upload/$uploadFileName");
+            }
+        }
         return $this->template($response, 'home.html.twig');
     }
 
