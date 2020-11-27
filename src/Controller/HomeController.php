084f12479bfee8532a31bda02afab4dbd1bb9473
Hakim <hak7s@hotmail.fr>
2020-11-27 15:05:25 +0100

    uploadfichier

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Driver\IBMDB2\Result;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -15,15 +16,46 @@ class HomeController extends AbstractController
             $newfile = $files['fichier'];
             if ($newfile->getError() === UPLOAD_ERR_OK) {
                 $uploadFileName = $newfile->getClientFilename();
-                $newfile->moveTo("upload/$uploadFileName");
+                $time = time();
+                $connection->executeStatement('INSERT INTO fichier (nom, nom_original) VALUES (:nom, :nom_original)', [
+                    'nom' => $time,
+                    'nom_original' => $uploadFileName,
+                ]);
+                $newfile->moveTo("../upload/$time");
             }
         }
         return $this->template($response, 'home.html.twig');
     }
 
-    public function download(ResponseInterface $response, int $id)
+    private function success(int $id, Connection $connection) // cette fonction devrait ce trouver dans une classe dediée
     {
-        $response->getBody()->write(sprintf('identifiant: %d', $id));
+        $query = $connection->prepare('SELECT * FROM fichier WHERE id = :id');
+        $query->bindValue('id', $id);
+        $result = $query->execute();
+
+        $fichier = $result->fetchAllAssociative();
+        return $fichier;
+    }
+
+    public function download(ResponseInterface $response, Connection $connection, int $id)
+    {
+        $fichier = static::success($id, $connection);
+        if (!empty($fichier)) {
+            $file_name = $fichier[0]['nom_original'];
+            $path = "../upload/$fichier[0]['nom']";
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename="' . basename($file_name) . '"');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($path));
+            flush(); // Flush system output buffer
+            readfile($path);
+            die;
+        } else {
+            $response->getBody()->write(sprintf('fichier non-trouver'));
+        }
         return $response;
     }
 }
